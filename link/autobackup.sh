a1bfbd5a6b29166ca6be5034de7ab95483b18339
yushiuan9499 <yushiuan9499@gmail.com>
2025-01-07 20:29:57 +0800

    Auto 2025-01-07-20:29

--- link/autobackup.sh
+++ link/autobackup.sh
@@ -13,8 +13,22 @@ for dir in $TARGET_DIR; do
     fi
     if [ -n "$(git status --porcelain)" ]; then
         echo "Directory $dir is not clean."
+        read -p "Do you want to commit? [y/n] " answer
+        answer=${answer:-y} # Default answer is y
+        if [ "$answer" != "y" ]; then
+            echo "Aborted."
+            exit 1
+        fi
         git add .
-        git commit -m "Auto $(date +%F-%H%M)"
+        read -p "Enter commit message: " message
+        message=${message:-Auto $(date +%F-%H:%M)} # Default message is Auto date-time
+        git commit -m "$message"
+        read -p "Do you want to push? [y/n] " answer
+        answer=${answer:-y} # Default answer is you
+        if [ "$answer" != "y" ]; then
+            echo "Aborted."
+            exit 1
+        fi
         git push origin master
     fi
 done
---
+++ link/synch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+TARGET_DIR=("$HOME/.config")
+
+for dir in $TARGET_DIR; do
+    if [ ! -d $dir ]; then
+        echo "Directory $dir does not exist."
+        exit 1
+    fi
+    cd $dir || exit 1
+    if [ ! -d .git ]; then
+        echo "Directory $dir is not a git repository."
+        exit 1
+    fi
+    echo "fetching $dir"
+    git fetch origin master >/dev/null 2>&1   # > /dev/null 2>&1 is used to suppress output
+    LOCAL=$(git rev-parse HEAD)               # Get the local commit hash
+    REMOTE=$(git rev-parse origin/master)     # Get the remote commit hash
+    BASE=$(git merge-base HEAD origin/master) # Get the common ancestor commit hash
+    if [ "$LOCAL" = "$REMOTE" ]; then
+        echo "Directory $dir is up-to-date."
+    elif [ "$LOCAL" = "$BASE" ]; then
+        echo "Directory $dir needs to pull."
+        read -p "Do you want to pull? [y/n] " answer
+        answer=${answer:-y} # Default answer is y
+        if [ "$answer" != "y" ]; then
+            echo "Aborted."
+            exit 1
+        fi
+        git pull origin master
+    else
+        echo "Directory $dir has diverged."
+    fi
+done
