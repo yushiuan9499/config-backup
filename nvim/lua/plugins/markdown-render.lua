2d1e55e0377e8238ce0b70c01ea718f4aeba706f
yushiuan9499 <yushiuan9499@gmail.com>
2025-04-23 18:50:57 +0800

    add latex render

--- nvim/lua/plugins/markdown-render.lua
+++ nvim/lua/plugins/markdown-render.lua
@@ -1,9 +1,11 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {},
+  { "ryleelyman/latex.nvim", opts = {} },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+    opts = {
+      latex = { enabled = false },
+      win_options = { conceallevel = { rendered = 2 } },
+    },
+  },
 }
